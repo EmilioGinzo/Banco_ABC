ddeac44bae30aad359e406edb3cb62403b5fa620
Emilio <emilioginzo@fpuna.edu.py>
2021-09-25 19:36:35 -0400

    create a document for each client with the correct information (name, account and balance)

--- Script/main.py
+++ Script/main.py
@@ -1,20 +1,33 @@
 import pandas as pd
+from docxtpl import DocxTemplate
+from pandas.core.frame import DataFrame
 
-df = pd.read_excel("Input\Datos Clientes.xlsx",sheet_name = "Sheet1", header = 0)
+#recibe el nombre de un cliente y retorna un diccionario con el nombre del cliente, el numero de cuenta y el saldo y la suma de todos los saldos
+def datos_cliente(nombre_cliente: str, df: DataFrame) -> object:
+    cuenta = df.loc[df.Cliente == nombre_cliente, ["N de Cuenta"]].to_string(index = False, header = False)
+    saldo = df.loc[df.Cliente == nombre_cliente, ["Saldo"]].to_string(index = False, header = False)
+    contenido = {
+        'nombre_cliente': nombre_cliente,
+        'numero_de_cuenta': cuenta,
+        'saldo': saldo,}
+    return contenido 
 
-#imprime los datos (numero de cuenta, saldo, suma de saldo )del cliente que se le pase
-def datos_cliente(nombre_cliente: str) -> None:
-    print("datos del cliente ", nombre_cliente)
-    print(df.loc[df.Cliente == nombre_cliente, ["N de Cuenta", "Saldo"]].to_string(index = False, header = True))
-    suma = df.loc[df.Cliente == nombre_cliente, ["Saldo"]].sum()
-    print("\nTotal", suma)
+def crear_docx(nombre_cliente: str, df: DataFrame) -> None:
+    print("creando documentos de " + nombre_cliente)
+    doc = DocxTemplate("Output\BANCO ABC - Carta.docx")
 
-#imprime los datos separados de cada cliente
-def cuentas_clientes(clientes: object) -> None:
+    contenido = datos_cliente(nombre_cliente, df)
+    doc.render(contenido)
+    doc.save("Output\BANCO ABC - Carta " + nombre_cliente + ".docx")
+
+
+def cuentas_clientes(df: DataFrame) -> None:
     prev_cliente = ""
-    for cliente in clientes:
+    for cliente in df.Cliente:
         if prev_cliente != cliente:
-            datos_cliente(cliente)
+            crear_docx(cliente, df)
             prev_cliente = cliente
 
-cuentas_clientes(df.Cliente)
+if __name__ == "__main__":
+    df = pd.read_excel("Input\Datos Clientes.xlsx",sheet_name = "Sheet1", header = 0)
+    cuentas_clientes(df)
